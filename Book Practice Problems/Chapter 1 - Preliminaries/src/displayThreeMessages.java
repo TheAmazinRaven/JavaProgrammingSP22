2bd7bc5acb9662f60aaa230e749f4badeb255d7c
Raven Dickerson <raejdickerson@gmail.com>
2022-01-07 11:48:45 -0600

    I hope this works

---
+++ Book Practice Problems/Chapter 1 - Preliminaries/src/displayThreeMessages.java
@@ -0,0 +1,14 @@
+public class displayThreeMessages {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Computer Science!");
+        System.out.println("Programming is fun, except for when it's not because your code won't compile.");
+
+    }
+}
+
+/*- `Welcome to Java`
+- `Welcome to Computer Science`
+- `Programming is Fun`
+ */
