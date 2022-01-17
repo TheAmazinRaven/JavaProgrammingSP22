ea187f4ca431a0c49fb48849741044dfcdc06452
Raven Dickerson <raejdickerson@gmail.com>
2022-01-16 18:03:51 -0600

    started on homework for first assignment

---
+++ Homework and Programming Assignments/Homework 1/src/question2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class question2 {
+    public static void main(String[] args) {
+
+        // declare variables
+
+        String name, major;
+        char gender;
+        int age;
+
+        // open the scanner
+        Scanner input = new Scanner(System.in);
+
+        // declare scanner inputs as variables
+        System.out.println("What is your name?");
+        name = input.nextLine();
+
+        System.out.println("How old are you?");
+        age = input.nextInt();
+
+        System.out.println("What is your gender? (W) - Woman, (M) - Man, (N) - Non-binary.");
+        gender = input.next().charAt(0);
+
+
+        // why doesn't this work?
+
+        System.out.println("What is your major?");
+        major = input.next();
+
+
+        // print user input
+        System.out.println("Hi " + name + "! You are " + age + " years old. Your gender is " + gender + ". & Your major is " + major + "!");
+    }
+}
+
+/*
+Write a Java program that lets a user enter their name, gender, major and age and then print to the IDE console the user entries each on a line.
+ */
---
+++ Homework and Programming Assignments/Homework 1/src/question3.java
@@ -0,0 +1,26 @@
+//import java.util.Scanner;
+//
+//public class question3 {
+//    public static void main(String[] args) {
+//
+//        // declare variables
+//        double celcius = (5.0 / 9 * ( fahrenheit - 32));
+//
+//        // open scanner
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter a degree in Fahrenheit: ");
+//
+//    }
+//}
+
+/*
+Write a Java program that converts a Fahrenheit to degree Celsius using the formula:
+
+Let the user input the temperature to be converted and use the appropriate datatype to hold it. Then do your conversion using the below formula.
+
+Celsius = 5.0/9 * (Fahrenheit – 32)
+
+Print to the IDE console the Celsius equivalent of the user Fahrenheit temperature input.
+ */
